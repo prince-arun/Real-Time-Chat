15d1a02c8f60ef66d67ca701ad917511aec60d7b
prince-arun <prince.skillsafari@gmail.com>
2023-08-09 04:29:09 +0530

    Routing application completed

--- real-time-chat/src/App.js
+++ real-time-chat/src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Components/Home/Home";
 import SignIn from "./Components/Home/SignIn";
+import Homepage from "./Components/Rest/Homepage";
+import AuthProvider from "./context/auth";
+import Profile from "./Components/Rest/Profile";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <SignIn />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
