23fb95b8afb5965fe0cc71154ce4285825dc48c0
prince-arun <prince.skillsafari@gmail.com>
2023-08-09 04:17:48 +0530

    Message section completed

---
+++ real-time-chat/src/Components/Rest/Message.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from "react";
+import Moment from "react-moment";
+
+const Message = ({ msg, user1 }) => {
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [msg]);
+
+  const messageStyle = {
+    maxWidth: "60%", // Adjust this value as needed
+    overflowWrap: "break-word",
+  };
+  return (
+    <div
+      className={`message_wrapper ${msg.from === user1 ? "own" : ""}`}
+      ref={scrollRef}
+    >
+      <p className={msg.from === user1 ? "me" : "friend"} style={messageStyle}>
+        {msg.media ? <img src={msg.media} alt={msg.text} /> : null}
+        {msg.text}
+        <br />
+        <small>
+          <Moment fromNow>{msg.createdAt.toDate()}</Moment>
+        </small>
+      </p>
+    </div>
+  );
+};
+
+export default Message;
