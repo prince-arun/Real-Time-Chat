229bc772e723b35f3787314b6670425ece2bea24
prince-arun <prince.skillsafari@gmail.com>
2023-08-09 04:28:26 +0530

    validation added

--- real-time-chat/src/Components/Home/SignIn.js
+++ real-time-chat/src/Components/Home/SignIn.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../firebase";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { updateDoc, doc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
 
 const SignIn = () => {
+  //---------------------------------------
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    error: null,
+    loading: false,
+  });
+
+  const navigate = useNavigate();
+
+  const { email, password, error, loading } = data;
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setData({ ...data, error: null, loading: true });
+    if (!email || !password) {
+      setData({ ...data, error: "All fields are required" });
+    }
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+
+      await updateDoc(doc(db, "users", result.user.uid), {
+        isOnline: true,
+      });
+      setData({
+        email: "",
+        password: "",
+        error: null,
+        loading: false,
+      });
+      navigate("/home");
+    } catch (err) {
+      setData({ ...data, error: err.message, loading: false });
+    }
+  };
+
+  //---------------------------------------------------------
   return (
     <div className="signIn d-flex justify-content-center">
       <div className="signIn-box ">
@@ -13,7 +58,7 @@ const SignIn = () => {
               <h2>Login</h2>
             </div>
             <div className="mx-5">
-              <Form>
+              {/* <Form onSubmit={handleSubmit}>
                 <Form.Group
                   className="mb-3"
                   controlId="exampleForm.ControlInput1"
@@ -23,6 +68,8 @@ const SignIn = () => {
                     type="email"
                     placeholder="name@example.com"
                     autoFocus
+                    value={email}
+                    onChange={handleChange}
                   />
                 </Form.Group>
                 <Form.Group
@@ -34,14 +81,45 @@ const SignIn = () => {
                     type="password"
                     placeholder="name@example.com"
                     autoFocus
+                    value={password}
+                    onChange={handleChange}
                   />
                 </Form.Group>
-              </Form>
-              <div className="text-center ">
-                <Button className="  btn-submit-sign">Sign In</Button>
-              </div>
+                {error ? <p className="error">{error}</p> : null}
+
+                <div className="text-center ">
+                  <Button className="  btn-submit-sign">Sign In</Button>
+                </div>
+              </Form> */}
+              {/* ----------------------------- */}
+              <form className="form" onSubmit={handleSubmit}>
+                <div className="input_container">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="input_container">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                  />
+                </div>
+                {error ? <p className="error">{error}</p> : null}
+                <div className="btn_container">
+                  <button className="btn" disabled={loading}>
+                    {loading ? "Logging in ..." : "Login"}
+                  </button>
+                </div>
+              </form>
               <Form.Label className="text-center mt-3 ms-5  ">
-                Don't have an account Register
+                Don't have an account <Link to="/">Register</Link>
               </Form.Label>
             </div>
           </Card.Body>
